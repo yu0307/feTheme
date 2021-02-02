da10458d88bc33444223a4f915ed9f0379a5d1d8
Lucas <yu0307@gmail.com>
2021-02-02 00:47:54 -0800

    ps

--- src/resources/js/frame.js
+++ src/resources/js/frame.js
@@ -10,12 +10,49 @@ function toggleControl(event) {
     }
 }
 
+function scrollControl(event){
+    let data = (()=>{
+        switch(this.attributes.getNamedItem('data-direction').value){
+            case"up":
+                return { y : "-=20" };
+            case"down":
+                return { y : "+=20" };
+            case"left":
+                return { x : "-=20" };
+            case"right":
+                return { x : "+=20" };
+        }
+    })();
+    OverlayScrollbars(this.closest(".custom-scroll").querySelector(".scroll-container")).scroll(data,400);
+}
+
+function toggleNavButton(e){
+    let base = this.getElements().host.closest('.custom-scroll');
+    base.querySelectorAll('.scroll-x').forEach((elm)=>{
+        if(e.xScrollable) elm.classList.add('show');
+        else elm.classList.remove('show');
+    });
+    base.querySelectorAll('.scroll-y').forEach((elm)=>{
+        if(e.yScrollable) elm.classList.add('show');
+        else elm.classList.remove('show');
+    });
+}
+
 function documentReady() {
     document.querySelectorAll('.toggleTarget').forEach((elm)=>{
         elm.addEventListener('click',toggleControl);
     });
     document.querySelectorAll('.custom-scroll').forEach((elm)=>{
-        OverlayScrollbars(elm, { className : "os-theme-dark",scrollbars:{autoHide : "leave"} });
+        OverlayScrollbars(elm.querySelector('.scroll-container'), { 
+            className : "os-theme-dark",
+            scrollbars:{autoHide : "leave"},
+            callbacks:{
+                onOverflowChanged:toggleNavButton
+            }
+        });
+        elm.querySelectorAll('.menu-direction-control').forEach((elm)=>{
+            elm.addEventListener('click',scrollControl);
+        })
     });
 }
 
--- src/resources/views/topbar.blade.php
+++ src/resources/views/topbar.blade.php
@@ -3,20 +3,14 @@
         <div class="toggleTarget d-md-none border-0" tg-target="#sidebar-left">
             <i class="fas fa-bars fa-lg" onclick="window.fe_frame.toggleClass(this,'fa-times')"></i>
         </div>
-        <div class="ms-2 nav-controls">
-            <div class="custom-scroll nav-container px-0">
+        <div class="ms-2 custom-scroll nav-controls d-flex">
+            <div class="menu-direction-control scroll-x align-self-center mx-1" data-direction="left"><i class="fas fa-lg fa-chevron-left"></i></div>
+            <div class="nav-container scroll-container px-0">
                 <ul class="px-0 mx-0 nav-icons mb-1">
                     @yield('topbar_menus')
-                    <!-- testing remove on production -->
-                    <li><i class="fab fa-lg fa-cloudversify"></i></li>
-                    <li><i class="fab fa-lg fa-cloudversify"></i></li>
-                    <li><i class="fab fa-lg fa-cloudversify"></i></li>
-                    <li><i class="fab fa-lg fa-cloudversify"></i></li>
-                    <li><i class="fab fa-lg fa-cloudversify"></i></li>
-                    <li><i class="fab fa-lg fa-cloudversify"></i></li>
-                    <li><i class="fab fa-lg fa-cloudversify"></i></li>
                 </ul>
             </div>
+            <div class="menu-direction-control scroll-x align-self-center mx-1" data-direction="right"><i class="fas fa-lg fa-chevron-right"></i></div>
         </div>
     </div>
     <div class="col justify-content-end d-flex">
