34dfe50b443742e14ea10d043406f666459f5bbd
Lucas <yu0307@gmail.com>
2021-03-05 20:51:52 -0800

    Minor cosmetic fix for top bar menus

--- src/resources/views/topbar.blade.php
+++ src/resources/views/topbar.blade.php
@@ -9,14 +9,15 @@
         <div class="toggleTarget d-md-none border-0" tg-target="#sidebar-left">
             <i class="fas fa-bars fa-lg" onclick="window.fe_frame.toggleClass(this,'fa-times')"></i>
         </div>
-        <div class="ms-2 custom-scroll nav-controls d-flex">
-            <div class="menu-direction-control scroll-x align-self-center mx-1" data-direction="left"><i class="fas fa-lg fa-chevron-left"></i></div>
-            <div class="nav-container scroll-container px-0">
+        <div class="ms-2 custom-scroll nav-controls">
+            <div class="d-inline-block menu-direction-control scroll-x align-self-center mx-1" data-direction="left"><i class="fas fa-lg fa-chevron-left"></i></div>
+            <div class="d-inline-block nav-container scroll-container px-0">
                 <ul class="px-0 mx-0 nav-icons mb-1">
+                    {{-- Example menu <li><i class="fab fa-internet-explorer"></i></li> --}}
                     @yield('topbar_menus')
                 </ul>
             </div>
-            <div class="menu-direction-control scroll-x align-self-center mx-1" data-direction="right"><i class="fas fa-lg fa-chevron-right"></i></div>
+            <div class="d-inline-block menu-direction-control scroll-x align-self-center mx-1" data-direction="right"><i class="fas fa-lg fa-chevron-right"></i></div>
         </div>
     </div>
     <div class="col justify-content-end d-flex">
